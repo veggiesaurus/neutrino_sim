9669f4cb1b9644b3f33d1c90ef26c97386ec03a7
Angus <accomrie@gmail.com>
2013-04-22 15:38:28 +0200

    update simulation command line call

--- simulate.sh
+++ simulate.sh
@@ -2,10 +2,10 @@
 MIN_ENERGY=60
 MAX_ENERGY=60
 DELTA_ENERGY=1
-MIN_BORON=230
-MAX_BORON=249
-DELTA_BORON=1
-NUM_EVENTS=30000
+MIN_BORON=100
+MAX_BORON=1000
+DELTA_BORON=100
+NUM_EVENTS=100000
 
 
 ENERGY=$MIN_ENERGY
@@ -18,7 +18,7 @@ until [  $ENERGY -gt $MAX_ENERGY ]; do
 		FILENAME="neutron_capture_pos_"$ENERGY"keV_"$BORON"pc.txt"
 		echo $FILENAME writing
 		#echo "Step#    X(mm)    Y(mm)    Z(mm) KinE(MeV)  dE(MeV) StepLeng TrackLeng  NextVolume ProcName">$FILENAME
-		./exampleN01 --neutronEnergy $ENERGY --neutronEnergySpread 0 --numberOfEvents $NUM_EVENTS --edgeCells 3 --boronLoading $BORON --boronEnrichment 100|grep alpha -A4 |grep initStep >$FILENAME
+		./neutrino --neutronEnergy $ENERGY --neutronEnergySpread 0 --numberOfEvents $NUM_EVENTS --edgeCells 3 --boronLoading $BORON --boronEnrichment 100|grep alpha -A4 |grep initStep >$FILENAME
 		let BORON+=$DELTA_BORON	
 	done
 
